2786a944da745798f8e2450f1f36c060075b5738
Mabel Shah <43481442+mabelshah@users.noreply.github.com>
2019-10-29 00:50:20 -0700

    Add files via upload

--- terraform.tfvars
+++ terraform.tfvars
@@ -1,7 +1,16 @@
-candidate = "" #your name
-# ...
-# ...
-# ...
+candidate =
+ "mabel"
+
 tags = {
   Name = "case-study-test"
 }
+
+ami = "ami-07c307e59cd2f19ec"
+
+instance_type =  "t3.medium"
+
+key_name = "mabel"
+
+subnet_id =  "subnet-0271f8735ed0d9284"
+
+vpc_security_group_ids = ["sg-02073367768e306bb"]
--- variables.tf
+++ variables.tf
@@ -1,1 +1,30 @@
-# ...
+# Variables
+
+variable "ami" {
+}
+
+variable "instance_type" {
+ }
+
+variable "key_name" {
+   }
+
+variable "subnet_id" {
+}
+
+variable "vpc_security_group_ids" {
+  type = "list"
+}
+
+variable "candidate" {
+}
+
+variable "tags" {
+  type = "map"
+}
+
+variable "secret_key" {
+}
+
+variable "access_key" {
+}
